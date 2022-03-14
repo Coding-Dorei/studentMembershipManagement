a32c62cc9941dca9a9c06b43ef70547deda9c486
Coding-Dore <lkjuio3865@naver.com>
2022-03-14 13:50:45 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const xlsx = require('xlsx')
+/**
+ * 
+ * @param {String} fileName 
+ * @returns jsonData
+ */
+function readExcel(fileName){
+    excelFile = xlsx.readFile(fileName)
+    sheetName = excelFile.SheetNames[0]
+    firstSheet = excelFile.Sheets[sheetName]
+    return xlsx.utils.sheet_to_json(firstSheet,{defval:""})
+}
+
+const 납부자명단 = readExcel("2022학생회비납부자.xlsx")
+const 통장거래내역 = readExcel("통장거래내역.xlsx")
+
+for(i = 0;i < 통장거래내역.length;i++){
+    if(통장거래내역[i].맡기신금액 == 160000){
+        let name = 통장거래내역[i].기재내용
+        for(j = 납부자명단.length-1;j>=0;j--){
+            if(납부자명단[j].name == name){
+                console.log(name)
+                납부자명단[j].level = 1
+                break
+            }
+        }
+    }
+}
+
+const book = xlsx.utils.book_new()
+
+const students = xlsx.utils.json_to_sheet(납부자명단)
+
+xlsx.utils.book_append_sheet(book,students,"시트1")
+
+xlsx.writeFile(book,"updated.xlsx")
